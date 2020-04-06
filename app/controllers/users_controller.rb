7010cc2b73789803b562efb985b1bf5f9260aec7
takedatsutomu <mdhd.to@gmail.com>
2020-04-06 11:13:16 +0900

    プロフィール画像

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    # binding.pry
+    @user = User.find(params[:id])
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :posts, dependent: :destroy
+         attachment :profile_image, destroy: false
 
 end
