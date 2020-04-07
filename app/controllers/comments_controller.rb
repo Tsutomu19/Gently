12ddd9fdbd65c3d5102e9d71424aee8463385d7d
takedatsutomu <mdhd.to@gmail.com>
2020-04-08 08:51:48 +0900

    [update]コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_back(fallback_location: posts_path)
   end
   private
 
