c76295d878c854f744706b81686cc2f5973bf6f8
takedatsutomu <mdhd.to@gmail.com>
2020-04-08 20:01:19 +0900

    [Add]非同期いいね

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,21 @@
 class FavoritesController < ApplicationController
 
+  before_action :set_variables # 非同期いいね、名称変更しない
+
   def create
     favorite = current_user.favorites.build(post_id: params[:post_id])
     favorite.save
-    redirect_back(fallback_location: posts_path)
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user, post_id: params[:post_id])
     favorite.destroy
-    redirect_back(fallback_location: posts_path)
   end
+
+  private
+
+  def set_variables
+    @post = Post.find(params[:post_id])
+  end
+
 end
