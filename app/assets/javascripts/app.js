b7a353b9014ff3d62f4fdb6f9ac5d9a9970aba22
takedatsutomu <mdhd.to@gmail.com>
2020-06-25 13:14:00 +0900

    [update]コメント消去

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,32 +1,32 @@
 
 $(function() {
 
-    var btn = document.getElementById('btn');　          //要素を取得　シングルクォーテーションはnew側のbtnだと思う
+    var btn = document.getElementById('btn');
     var content = document.getElementById('content');
 
-    var speech = new webkitSpeechRecognition();　        //音声認識APIの使用 speechオブジェクトを取得する
+    var speech = new webkitSpeechRecognition();
 
     speech.lang = "ja";
 
-    btn.addEventListener('click' , function() {　          //クリックしたら次の操作を行いなさい
-        // btn.disabled = true;                               //2度押しを防ぐ
+    btn.addEventListener('click' , function() {
+        // btn.disabled = true;
         speech.start();
     });
 
-    speech.addEventListener('result', function(e) {　
+    speech.addEventListener('result', function(e) {
         $.ajax({
             url: '/posts/new',
             type: 'GET',
             date: {
-                content: text　　// JSの変数の中のデータをRailsに渡す。変数text
+                content: text
             },
         })
 
-        var text = e.results[0][0].transcript;　           // 認識された「言葉」を、変数「text」に格納
+        var text = e.results[0][0].transcript;
 
-        content.textContent = text;　                      // 認識された「言葉(text)」を、表示用のdivタグに代入する  textContentプロパティはテキスト内容の表示
+        content.textContent = text;
 
-        document.getElementById("speech").value = text;     // domを使ってformへ
+        document.getElementById("speech").value = text;
 
 
     });
